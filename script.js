37c63dff9a824607906a544ec9b26033109328ca
SamuelCandinho <samuel_e_candinho@estudante.sesisenai.org.br>
2026-04-14 22:53:44 -0300

    fix years and name

--- script.js
+++ script.js
@@ -9,25 +9,29 @@ document.getElementById("docaosangue").addEventListener("submit", function (e) {
     let idade = document.getElementById("idade").value;
 
     let verinome = nome.split(" ");
-    let idadever = idade.split("/");
+    let idadever = idade.split("-");
 
     let sobrenome = verinome[2];
-    let idade2 = idadever[2];
-
-    let realidade = 2026 - idade2;
-    if (realidade < 15) {
-        alert ("tenha 16 anos");
+    let idade2 = idadever[0];
+    let dataatual = new Date().getFullYear();
+    let realidade = dataatual - idade2;
+    if (realidade < 16) {
+        alert ("Você deve ter pelo menos 16 anos");
         return;
     }
     if (peso < 49) {
         alert ("você não tem peso necessario");
         return;
     }
-    if (sobrenome.length < 3) {
+    if (sobrenome.length < 2) {
         alert("Por favor, insira seu nome completo.");
         return;
     }
+    else {
+          alert("deu tudo certo parabens")
+
+    }
     
 
-    alert("deu tudo certo parabens")
+  
 });
